a05edffa3259f465bb2b92f6a01746dfe9a6cd7e
东往 <wgzhxy@163.com>
2018-02-08 17:50:33 +0800

    创建dynamic模块

---
+++ dynamic/src/main/java/com/yisutech/framework/dynamic/App.java
@@ -0,0 +1,10 @@
+package com.yisutech.framework.dynamic;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
